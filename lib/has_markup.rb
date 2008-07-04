f12c65367c7232cf822f2839a40a41f0016b9fc7
Josh Nichols <josh@technicalpickles.com>
2008-07-04 00:57:39 -0400

    FIRST COMMIT

---
+++ lib/has_markup.rb
@@ -0,0 +1,69 @@
+module HasMarkup
+  def self.included(base)
+    base.extend ClassMethods
+  end
+  
+  class UnsupportedSyntax < RuntimeError
+    attr_accessor :syntax
+    def initialize(syntax)
+      @syntax = syntax
+    end
+  end
+  
+  module ClassMethods
+    def has_markup(attr, options = {})
+      include HasMarkup::InstanceMethods
+      
+      options = default_has_markup_options.merge(options)
+      
+      if options[:required]
+        validates_presence_of attr
+      end
+      
+      case syntax = options[:syntax]
+      when :markdown
+        validates_markdown_syntax attr
+      else
+        raise "Unsupported syntax #{syntax.inspect}. has_markup supports: :markdown"
+      end
+      
+      if options[:cache]
+        define_method "cache_#{attr}" do
+          cache_markup_of attr, options[:syntax]
+        end
+        before_save "cache_#{attr}".to_sym
+      end
+    end
+    
+    def validates_markdown_syntax(*attrs)
+      validates_each(*attrs) do |record, attr, value|
+        begin
+          BlueCloth.new(value).to_html unless value.nil?
+        rescue BlueCloth::FormatError => e
+          errors.add attr, "has #{e}" 
+        end
+      end
+    end
+    
+    protected
+        def default_has_markup_options
+          {
+            :syntax => :markdown,
+            :required => false,
+            :cache => false
+          }
+        end
+  end
+  
+  module InstanceMethods
+    def cache_markup_of(attr, syntax)
+      case syntax
+      when :markdown
+        html = BlueCloth.new(self.send(attr)).to_html
+        self.send("cached_#{attr}=", html)
+      else
+        raise "Unsupported syntax #{syntax.inspect}. has_markup supports: :markdown"
+      end
+    end
+  end
+end
