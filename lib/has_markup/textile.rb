8d6b9030e8bb4cbcc9f9e0d4093d6b57039a92e5
Josh Nichols <josh@technicalpickles.com>
2008-08-04 19:17:53 -0400

    Added initial textile support.

---
+++ lib/has_markup/textile.rb
@@ -0,0 +1,36 @@
+module HasMarkup
+  module Textile
+    # Sprinkles the magic needed to support textile. In particular, it will define a method <tt>column_html</tt> which uses RedCloth to generate HTML.
+    def sprinkle_textile_magic(column)
+      require 'redcloth'
+    
+      extend HasMarkup::Textile::ClassMethods
+      include HasMarkup::Textile::InstanceMethods
+    
+      validates_markdown_syntax column
+      define_method "#{column}_html" do
+        markdown = self.send(column)
+        self.generate_html_from_markdown(markdown)
+      end
+    end
+    
+    module InstanceMethods
+      def generate_html_from_markdown(textile)
+        RedCloth.new(textile).to_html unless textile.blank?
+      end
+    end
+    
+    module ClassMethods
+      # Validates the markdown syntax of the given columns.
+      def validates_textile_syntax(*columns)
+        validates_each(*columns) do |record, column, value|
+          begin
+            record.generate_html_from_markdown(value)
+          rescue BlueCloth::FormatError => e
+            errors.add column, "has #{e}"
+          end
+        end
+      end
+    end
+  end
+end
